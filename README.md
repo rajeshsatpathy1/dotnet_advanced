3cb5325b210bddb402fd0cea6fe07baf3ab7f88f
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2024-02-14 00:52:12 -0600

    Add Generics

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotnet_advanced
 A repo to explore the dotnet advanced tutorial (https://www.udemy.com/course/csharp-advanced/) to using chatgpt as the prompting solver.
+
+## Structure and Aim
+Each advanced topic is stored as <topic.cs>. The aim of each will be to prompt and have the code cover all the necessary concepts in the topic using game terms.
---
+++ topics/generics.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+public class Player
+{
+    public string Name { get; set; }
+}
+
+public class Enemy
+{
+    public string Type { get; set; }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+}
+
+public class GameObjectList<T>
+{
+    private List<T> list = new List<T>();
+
+    public void Add(T item)
+    {
+        list.Add(item);
+    }
+
+    public T Get(int index)
+    {
+        return list[index];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GameObjectList<Player> playerList = new GameObjectList<Player>();
+        playerList.Add(new Player { Name = "Player1" });
+
+        GameObjectList<Enemy> enemyList = new GameObjectList<Enemy>();
+        enemyList.Add(new Enemy { Type = "Enemy1" });
+
+        GameObjectList<Item> itemList = new GameObjectList<Item>();
+        itemList.Add(new Item { Name = "Item1" });
+
+        Console.WriteLine("Player: " + playerList.Get(0).Name);
+        Console.WriteLine("Enemy: " + enemyList.Get(0).Type);
+        Console.WriteLine("Item: " + itemList.Get(0).Name);
+    }
+}
+
+// This program creates lists of Player, Enemy, and Item objects, adds an object to each list,
+// and then prints the name or type of the first object in each list. You should see the names 
+// “Player1”, “Enemy1”, and “Item1” printed to the console when you run this program.
